9e034c67432666ab15d42505ac71d01d21362900
dhiaboubaker <dhia.4iot2025@gmail.com>
2024-10-09 23:46:32 +0100

    Add_Module
    Client : home, about
    Admin: homeadm, gestprod

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientModule } from './client/client.module';
+import { AdminModule } from './admin/admin.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ClientModule,
+    AdminModule
+  ]
+})
+export class AppModule { }
